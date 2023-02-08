1128e68e921c974f873450b38e6706dc5ba14c9f
carlosapplyrodriguez <carlos.apply.rodriguez@gmail.com>
2023-02-08 15:20:05 -0700

    imind/DINJECT-2 - Adding @Qualifier to Controllers in order to specify which service from many implementing the same interface, should be used.

--- src/main/java/com/imindloop/springdependencyinjection/controllers/ControllerWithPropertyBasedInjection.java
+++ src/main/java/com/imindloop/springdependencyinjection/controllers/ControllerWithPropertyBasedInjection.java
@@ -2,11 +2,13 @@ package com.imindloop.springdependencyinjection.controllers;
 
 import com.imindloop.springdependencyinjection.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ControllerWithPropertyBasedInjection {
 
+    @Qualifier("greetingServiceEnglish")
     @Autowired
     public GreetingService greetingService;
 
--- src/main/java/com/imindloop/springdependencyinjection/controllers/ControllerWithSetterBasedInjection.java
+++ src/main/java/com/imindloop/springdependencyinjection/controllers/ControllerWithSetterBasedInjection.java
@@ -2,15 +2,17 @@ package com.imindloop.springdependencyinjection.controllers;
 
 import com.imindloop.springdependencyinjection.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class ControllerWithSetterBasedInjection {
 
+    @Qualifier("greetingServiceGerman")
     @Autowired
     private GreetingService greetingService;
 
-    public void setGreetingService(GreetingService greetingService) {
+    public void setGreetingService(@Qualifier("greetingServiceGerman") GreetingService greetingService) {
         this.greetingService = greetingService;
     }
 
